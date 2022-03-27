9b4fd28a38b9d53e0a79d7bc778178d31d378ee5
suyeon woo <50320497+yeonwooz@users.noreply.github.com>
2022-03-27 13:00:34 +0000

    constructor pattern refactoring - use the prototype

--- CreationalPatterns/ConstructorPatterns.js
+++ CreationalPatterns/ConstructorPatterns.js
@@ -8,11 +8,12 @@ function Human(name, age, occupation){ //ES5 function based constructor
   this.name = name; 
   this.age = age;
   this.occupation = occupation;
-  //defining a method inside the constructor function
-  this.describe = function(){
+}
+
+//defining a method inside the constructor function using the prototype
+Human.prototype.describe = function(){
     console.log(`${this.name} is a ${this.age} year old ${this.occupation}`);
   }
-}
 //creating a "person" object using the Human constructor
 //the constructor uses the arguments passed into it to
 //initialize the property values name, age and sex
